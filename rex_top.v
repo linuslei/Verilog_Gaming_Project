788a75aecd5e477eb183f39d3f6b3b1c7a2604bd
linuslei <linuslei@usc.edu>
2023-11-15 17:35:54 -0800

    added example

--- rex_top.v
+++ rex_top.v
@@ -24,11 +24,26 @@ wire board_clk, sys_clk; //VGA Clock
 wire Start, Restart, Jump, Duck, Pause;
 wire q_Start, q_Stop, q_Jump, q_Duck, q_Run, q_Pause;
 
+reg [27:0]	DIV_CLK;
+always @ (posedge ClkPort, posedge Reset)  
+begin : CLOCK_DIVIDER
+    if (Reset)
+		DIV_CLK <= 0;
+	else
+		DIV_CLK <= DIV_CLK + 1'b1;
+	end
+
+wire vga_clk;
+assign vga_clk=DIV_CLK[19]; //slower clock to drive the movement of objects on the vga screen
+
+
 assign {MemOE, MemWR, RamCS, QuadSpiFlashCS} = 4'b1111;
 
 BUFGP BUFGP1 (board_clk, ClkPort);
 
 assign Reset = BtnC;
+
+
 
 always @(posedge board_clk, posedge Reset)
 begin
